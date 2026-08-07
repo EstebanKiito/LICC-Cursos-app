ae3ad8f48257a3b6aaab673161c0a97e25a6ef5c
EstebanKiito <esteban.ortega@uc.cl>
2026-08-07 11:58:51 -0400

    refactor: renombrar campos en modelo Course para mayor claridad

--- src/models/Course.ts
+++ src/models/Course.ts
@@ -9,24 +9,40 @@ Course.init({
     autoIncrement: true,
     primaryKey: true,
   },
-  sigla: {
-    type: DataTypes.STRING, // Ejemplo: IIC2173
+  code: {
+    type: DataTypes.STRING, // Ejemplo: IIC1103
     allowNull: false,
     unique: true,
   },
-  nombre: {
+  name: {
     type: DataTypes.STRING,
     allowNull: false,
   },
-  descripcion: {
+  description: {
     type: DataTypes.TEXT,
+    allowNull: true,
+  },
+  prerequisites: {
+    type: DataTypes.STRING, 
+    allowNull: true, // Lo dejamos nulo si no tiene prerrequisitos
+  },
+  credits: {
+    type: DataTypes.INTEGER,
+    allowNull: true, // Usamos INTEGER ya que todos son números (10, 5)
+  },
+  parity: {
+    type: DataTypes.STRING, // 'both', 'odd', 'even', o null
+    allowNull: true,
+  },
+  type: {
+    type: DataTypes.STRING, // 'dcc', 'fmat', 'major', 'eti', etc.
     allowNull: true,
   }
 }, {
   sequelize,
   modelName: 'Course',
   tableName: 'courses',
-  timestamps: false, // Los ramos no suelen necesitar fecha de creación
+  timestamps: false,
 });
 
 export default Course;
